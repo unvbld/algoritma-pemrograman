aff902459fe9ddea88f8932f718704283eb71f74
Ayman Na <natanagara.ayman@gmail.com>
2023-04-12 23:05:53 +0700

    add week 12 class assignments

---
+++ W12_class/02_Lengkapi-Program.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+const NMAX int = 30
+
+type intArray struct {
+    tabInt [NMAX]int
+    N int
+}
+
+func main() {
+    var array1, array2 intArray
+
+    inputArray(&array1)
+    inputArray(&array2)
+    sort(&array1)
+    sort(&array2)
+
+    fmt.Println(isSimilar(array1, array2))
+}
+
+func inputArray(T *intArray) {
+    var x int
+
+    fmt.Scan(&x)
+
+    for x != 0 {
+        if T.N < NMAX {
+            T.tabInt[T.N] = x
+            T.N++
+        }
+
+        fmt.Scan(&x)
+    }
+}
+
+func sort(T *intArray) {
+    var idx int
+
+    for i := 0; i < T.N-1; i++ {
+        idx = i
+
+        for j := i+1; j < T.N; j++ {
+            if T.tabInt[idx] > T.tabInt[j] {
+                idx = j
+            }
+        }
+
+        T.tabInt[i], T.tabInt[idx] = T.tabInt[idx], T.tabInt[i]
+    }
+}
+
+func isSimilar(T1, T2 intArray) bool {
+    var i int
+
+    if T1.N != T2.N {
+        return false
+    }
+
+    for i = 0; i < T1.N; i++ {
+        if T1.tabInt[i] != T2.tabInt[i] {
+            return false
+        }
+    }
+
+    return true
+}
